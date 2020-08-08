fca462fd5ca9ff5e773af4f6ab825bb5c469aa1f
Sebastian Brear <ssbrear@ucdavis.edu>
2020-08-08 11:18:50 -0700

    working proof of concept for tastedive api

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,7 +4,7 @@
 // OMDb api call example:
 // http://www.omdbapi.com/?apikey=20106460&t=the+princess+bride
 
-var tasteURL =  "https://tastedive.com/api/similar?k=381371-Similar-W5E25EP7&q=";
+var tasteURL =  "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?k=381371-Similar-W5E25EP7&q=";
 
 var movieName = "";
 
@@ -18,5 +18,8 @@ tasteURL += movieName;
 $.ajax({
     url: tasteURL
 }).then(function(response) {
-    console.log(response)
+    console.log(response.Similar.Info[0]);
+    for (var i = 0; i < 5; i++) {
+        console.log(response.Similar.Results[i]);
+    }
 });
